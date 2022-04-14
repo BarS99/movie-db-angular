0bbf9acbb1f3deb80d906dd6efd38ffd99f05d46
Bartlomiej Swiech <bartlomiej.swiech@enginiety.com>
2022-04-14 14:58:06 +0200

    feat: add getLocalStorage function

--- src/app/shared/utilities.ts
+++ src/app/shared/utilities.ts
@@ -1,3 +1,14 @@
-export function dateToIso(date: string) {
+export function dateToIso(date: string): string {
     return new Date(date).toISOString();
+}
+
+export function getLocalStorage(key: string): any[] {
+    const favoriteStorage: null|string = localStorage.getItem('favorite');
+    let favoriteArray: number[] = [];
+
+    if (favoriteStorage !== null) {
+      favoriteArray = JSON.parse(favoriteStorage);
+    }
+
+    return favoriteArray;
 }
