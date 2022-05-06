5cbdac39914bed70a16e0219d2d25d2964b083f3
Bartlomiej Swiech <bartlomiej.swiech@enginiety.com>
2022-05-06 09:40:40 +0200

    refactor: remove reduntant imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -33,6 +34,7 @@ import { FiltersEffects } from './index/components/filters/state/filters.effects
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     NgbModule,
     FontAwesomeModule,
